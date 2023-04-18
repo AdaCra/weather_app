ef3867a2cffd4bb6f2176a5f8545c313d5c688da
Tori Luszczek <wuszczek@gmail.com>
2023-04-18 17:01:00 +0200

    form done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Form } from "./components/form";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [activities, setActivities] = useState({
+    name: "test",
+    isForGoodWeather: null,
+    id: 1,
+  });
+  function handleAddActivity(newActivity) {}
+  return <Form onAddActivity={handleAddActivity} />;
 }
 
 export default App;
---
+++ src/components/form/index.js
@@ -0,0 +1,27 @@
+export function Form(onAddActivity) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    onAddActivity(data);
+    e.target.reset();
+    e.target.elements.name.focus();
+
+    console.log(data);
+  };
+
+  return (
+    <>
+      <form className="form__activity-form" onSubmit={handleSubmit}>
+        <legend>Add new Activity!</legend>
+        <label htmlFor="name">Anything</label>
+        <input type="text" id="Activity" name="name" required />
+        <label htmlFor="isForGoodWeather">
+          Is this a good weather activity?
+        </label>
+        <input type="checkbox" id="isWeatherGood" name="isForGoodWeather" />
+        <button type="Submit">Submit Activity</button>
+      </form>
+    </>
+  );
+}
