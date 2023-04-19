07e6206fd077accbd7495cffaea06757bb20a4d7
Tori Luszczek <wuszczek@gmail.com>
2023-04-19 13:28:40 +0200

    commit

--- src/components/fetchWeather/index.js
+++ src/components/fetchWeather/index.js
@@ -1,10 +1,1 @@
 import { useState } from "react";
-
-export async function FetchWeather(weather) {
-  const URL = "https://example-apis.vercel.app/api/weather/arctic";
-  const response = await fetch(URL);
-  const weatherData = await response.json();
-  weather = weatherData.isGoodWeather;
-  console.log(weather);
-  return weather;
-}
